7f19d22825747baf9d3eb8b2fb340eca9592e44d
bartgerard <bart.gerard@gmail.com>
2019-07-22 16:58:52 +0200

    And Ready To Go

--- quiz/client/src/app/app.module.ts
+++ quiz/client/src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {registerLocaleData} from '@angular/common';
+import localeNl from '@angular/common/locales/nl'
 
 @NgModule({
   declarations: [
@@ -13,6 +15,11 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {
+}
+
+registerLocaleData(localeNl, 'nl')
